27866753da0c64310210408de92895e12eb433fe
ivy <1510350223@qq.com>
2021-08-24 13:59:08 +0800

    1家

---
+++ testpytorch.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/8/19 11:08 上午
+# @Author  : ivy_nie
+# @File            : testpytorch.py
+# @Reference       :  None
+# @Software        : PyCharm
+import tensorflow as tf
+print(tf.__version__)
