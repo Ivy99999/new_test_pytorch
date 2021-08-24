0cfc76dd718ad5a7a67ee1e04716b924e147a6ad
ivy <1510350223@qq.com>
2021-08-24 13:45:42 +0800

    1家

---
+++ test.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/8/19 12:03 上午
+# @Author  : ivy_nie
+# @File            : test.py
+# @Reference       :  None
+# @Software        : PyCharm
+print("all mac new")
+import tensorflow as tf
+print(tf.__version__)
